069bb90125a2097f8413b1d2c41fadca707fcdc1
ArthurBraga <abraga@vistacapital.com.br>
2020-09-03 20:43:30 -0400

    more funds

---
+++ Fund_Analysis/CorrelationMatrix.py
@@ -0,0 +1,41 @@
+import BloombergAPI_new
+
+
+
+
+
+
+
+
+FundList = [    "VISTMUL BZ Equity",
+                "STHFIC BZ Equity",
+                "KAPKFFF BZ Equity",
+                "SPXNIMF BZ Equity",
+                "BBMFIC BZ Equity",
+                "ABVRTFF BZ Equity",
+
+
+
+                ]
+
+FundNames = {
+                "VISTMUL BZ Equity": "VISTA MULTI"
+                "STHFIC BZ Equity":"ibiuna hedge STH FIC",
+                "KAPKFFF BZ Equity":"kapitalo kappa",
+                "SPXNIMF BZ Equity": "spx nimitz",
+                "BBMFIC BZ Equity": "bahia marau",
+                "ABVRTFF BZ Equity": "absolute vertez",
+
+
+
+
+
+Garde Dargagnan
+Canvas Enduro
+Kinea Chronos
+Legacy capital
+Adam Macro
+Occam Retorno Abs
+
+
+}

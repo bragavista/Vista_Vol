b023607d3736dd2665224ab52806293af6918875
ArthurBraga <abraga@vistacapital.com.br>
2020-09-04 14:04:55 -0400

    more funds and mappings

--- Fund_Analysis/CorrelationMatrix.py
+++ Fund_Analysis/CorrelationMatrix.py
@@ -1,41 +1,39 @@
 import BloombergAPI_new
 
-
-
-
-
-
-
-
 FundList = [    "VISTMUL BZ Equity",
                 "STHFIC BZ Equity",
                 "KAPKFFF BZ Equity",
                 "SPXNIMF BZ Equity",
                 "BBMFIC BZ Equity",
                 "ABVRTFF BZ Equity",
-
-
-
-                ]
+                "DARTFIC BZ Equity",
+                "ADVEII BZ Equity",
+                "CSH2086 BZ Equity",
+                "ADM1MCR BZ Equity",
+                "BRPLABF BZ Equity",
+                "VNLMADV BZ Equity"
+             ]
 
 FundNames = {
-                "VISTMUL BZ Equity": "VISTA MULTI"
-                "STHFIC BZ Equity":"ibiuna hedge STH FIC",
-                "KAPKFFF BZ Equity":"kapitalo kappa",
+                "VISTMUL BZ Equity": "VISTA MULTI",
+                "STHFIC BZ Equity":  "ibiuna hedge sth",
+                "KAPKFFF BZ Equity": "kapitalo kappa",
                 "SPXNIMF BZ Equity": "spx nimitz",
-                "BBMFIC BZ Equity": "bahia marau",
-                "ABVRTFF BZ Equity": "absolute vertez",
-
-
-
-
-
-Garde Dargagnan
-Canvas Enduro
-Kinea Chronos
-Legacy capital
-Adam Macro
-Occam Retorno Abs
-
+                "BBMFIC BZ Equity":  "bahia marau",
+                "ABVRTFF BZ Equity": "absolute vertex",
+                "DARTFIC BZ Equity": "garde d'artagnan",
+                "ADVEII BZ Equity":  "canvas enduro",
+                "MCRCHRN BZ Equity": "kinea chronos",
+                "CSH2086 BZ Equity": "legacy capital",
+                "ADM1MCR BZ Equity": "adam macro",
+                "BRPLABF BZ Equity": "occam retorno abs",
+                "VNLMADV BZ Equity": "vinland macro"
 
 }
+
+start_date   =   '20150101'
+end_date     =   '20200903'
+
+
+
+

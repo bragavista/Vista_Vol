77553d75a16a6e910427697f2cd7750febe7b0ca
ArthurBraga <abraga@vistacapital.com.br>
2020-10-19 22:53:02 -0400

    more stuff

--- Positioning/StockHoldings.py
+++ Positioning/StockHoldings.py
@@ -3,6 +3,7 @@ try:
 except:
     import Util.BloombergAPI_new as BloombergAPI
 import pandas as pd
+import numpy as np
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
@@ -47,20 +48,66 @@ def sort_by_position_change(all_holders_public_filiings,threshold_position_left_
 
 
 
+def do_all_related_stocks (RelatedStockList,reportingccy, pace):
+
+
+    blp = BloombergAPI.BLPInterface()
+    prices_map = blp.referenceRequest(RelatedStockList, "PX_LAST")
+    ccy_map =     blp.referenceRequest(RelatedStockList, "CRNCY")
+    unique_ccy_list = list(np.unique(ccy_map.values))
+    unique_ccy_list.remove(reportingccy)
+    fx_rates_list = [ccy+reportingccy+' Curncy' for ccy in unique_ccy_list]
+    fx_rates = blp.referenceRequest(fx_rates_list, "PX_LAST")
+
+    blp.close()
+
+    LiquidationStock = pd.DataFrame()
+
+    for StockTicker in RelatedStockList:
+
+        current_spot_px = prices_map[StockTicker]
+
+        ccy_den = ccy_map[StockTicker]
+
+        all_holders_public_filiings = fetch_holders_info(StockTicker)
+
+        active_big_holders = sort_by_position_change(all_holders_public_filiings, threshold_position_left_pct=0.10, sell_or_buy='sell')
+
+        liquidation_volume_last = active_big_holders[['Filing Date  ','Holder Name ','Percent Outstanding','Position ', 'Position Change  ', 'position_change_pct']]
+
+        liquidation_volume_last['StockTicker'] = StockTicker.replace(' Equity','')
+
+        if pace <0:
+            liquidation_volume_last = liquidation_volume_last.loc[liquidation_volume_last['position_change_pct'] < pace]
+        else:
+            liquidation_volume_last = liquidation_volume_last.loc[liquidation_volume_last['position_change_pct'] > pace]
+
+        if ccy_den==reportingccy:
+            fx_rate = 1
+        else:
+            fx_rate = fx_rates[ccy_den+reportingccy+' Curncy']
+
+        liquidation_volume_last['position_'+reportingccy] = round(liquidation_volume_last['Position '] * fx_rate * current_spot_px,0)
+
+        LiquidationStock = LiquidationStock.append(liquidation_volume_last)
+
+
+    return LiquidationStock
+
+
+
+
+
 
 if __name__ == "__main__":
 
-    StockTicker = "PETR3 BZ Equity"
 
-    all_holders_public_filiings = fetch_holders_info(StockTicker)
+    RelatedStockList = ["PETR4 BZ Equity","PETR3 BZ Equity","PBR US Equity","PBR/A US Equity"]
+    reportingccy = 'BRL'
+    pace = -0.1
 
-    active_big_holders = sort_by_position_change(all_holders_public_filiings, threshold_position_left_pct=0.10, sell_or_buy='sell')
+    LiquidationStock = do_all_related_stocks (RelatedStockList,'BRL',pace)
 
-    liquidation_volume_last = active_big_holders[['Filing Date  ','Holder Name ','Percent Outstanding','Position ', 'Position Change  ', 'position_change_pct']]
-
-    liquidation_pace = 0.1
-    liquidation_volume_last = liquidation_volume_last.loc[liquidation_volume_last['position_change_pct']< -liquidation_pace]
-
-    print(liquidation_volume_last)
-    print(StockTicker,'remaining stock of liquidation',round(liquidation_volume_last['Position '].sum()/1e6,1),'million shares of holders selling at a liquidation pace of ',-liquidation_pace,'of their previous holdings')
+    print(LiquidationStock)
+    print(RelatedStockList,'remaining value of liquidation',reportingccy,round(LiquidationStock['position_'+reportingccy].sum()/1e9,1),'billion of holders selling at a liquidation pace of ',pace,'of their previous holdings')
 

a3816fe55f955331913cbbb8192d404bde35e971
ArthurBraga <abraga@vistacapital.com.br>
2020-10-21 20:01:21 -0400

    more stuff

--- Positioning/StockHoldings.py
+++ Positioning/StockHoldings.py
@@ -1,9 +1,18 @@
 try:
     from Util import BloombergAPI_new as BloombergAPI
+    from Util import random_functions as random_functions
+    from Util import EmailSender as EmailSender
+
 except:
     import Util.BloombergAPI_new as BloombergAPI
+    import Util.random_functions as random_functions
+    import Util.EmailSender as EmailSender
+
+
 import pandas as pd
 import numpy as np
+from datetime import date
+
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
@@ -77,6 +86,8 @@ def do_all_related_stocks (RelatedStockList,reportingccy, pace):
 
         liquidation_volume_last['StockTicker'] = StockTicker.replace(' Equity','')
 
+        liquidation_volume_last['reportingccy'] = reportingccy
+
         if pace <0:
             liquidation_volume_last = liquidation_volume_last.loc[liquidation_volume_last['position_change_pct'] < pace]
         else:
@@ -87,31 +98,112 @@ def do_all_related_stocks (RelatedStockList,reportingccy, pace):
         else:
             fx_rate = fx_rates[ccy_den+reportingccy+' Curncy']
 
-        liquidation_volume_last['position_'+reportingccy] = round(liquidation_volume_last['Position '] * fx_rate * current_spot_px,0)
+        liquidation_volume_last['volume_in_reportingccy_mm'] = -round((liquidation_volume_last['Position '] * fx_rate * current_spot_px)/1e6,0)
 
         LiquidationStock = LiquidationStock.append(liquidation_volume_last)
 
 
 
-    LiquidationStock = LiquidationStock.sort_values(by=['Filing Date  ','position_BRL'],ascending=[False,True])
+    LiquidationStock = LiquidationStock.sort_values(by=['Filing Date  ','volume_in_reportingccy_mm'],ascending=[False,False])
 
 
     return LiquidationStock
 
 
+def capture_top_holdings_fund (FundName='TEMPOCA BZ Equity',n=10):
 
+    blp = BloombergAPI.BLPInterface()
+
+    top_holdings = blp.bulkRequest(FundName, "TOP_MUTUAL_FUND_HOLDINGS")
+
+    blp.close()
+
+
+    return top_holdings
+
+
+def do_all_stocks (target_stock_dict,pace=-0.1):
+
+    all_target_stocks = pd.DataFrame()
+    all_selling_stock = pd.DataFrame()
+
+    for stock in target_stock_dict.keys():
+
+        reportingccy = target_stock_dict[stock]['reportingccy']
+        RelatedStockList = target_stock_dict[stock]['RelatedStockList']
+        LiquidationStock = do_all_related_stocks(RelatedStockList,reportingccy, pace)
+        LiquidationStock['StockGroup'] = stock
+
+        Potential_Selling_Stock = LiquidationStock['volume_in_reportingccy_mm'].sum()
+
+        total_aux = pd.DataFrame(data=[Potential_Selling_Stock],index=[stock],columns=['PotentialLiquidation_' + reportingccy + '_mm'])
+
+        all_target_stocks = all_target_stocks.append([LiquidationStock,total_aux])
+
+        all_selling_stock = all_selling_stock.append(total_aux)
+
+
+    # print(RelatedStockList,'remaining value of liquidation',,LiquidationStock['position_'+reportingccy+'_mm'].sum(),'mm of holders selling at a liquidation pace of ',pace,'of their previous holdings')
+
+    return  all_target_stocks,all_selling_stock
+
+
+
+def recent_activity_calc(all_target_stocks,cutdate):
+
+    recent_activity = all_target_stocks.loc[all_target_stocks['Filing Date  ']>=cutdate]
+
+
+
+    recent_activity = recent_activity[['Filing Date  ','StockGroup','StockTicker','volume_in_reportingccy_mm','Holder Name ','Position ','Position Change  ','position_change_pct','reportingccy']]
+
+    return recent_activity
 
 
 
 if __name__ == "__main__":
 
+    cutdate = random_functions.prev_weekday(date.today())
 
     RelatedStockList = ["PETR4 BZ Equity","PETR3 BZ Equity","PBR US Equity","PBR/A US Equity"]
     reportingccy = 'BRL'
     pace = -0.1
 
-    LiquidationStock = do_all_related_stocks (RelatedStockList,'BRL',pace)
+    target_stock_dict = {
+                                'PETROBRAS' :  { 'reportingccy': 'BRL','RelatedStockList': ["PETR4 BZ Equity","PETR3 BZ Equity","PBR US Equity","PBR/A US Equity"]} ,
+                                'VALE' :       {'reportingccy':'BRL','RelatedStockList':["VALE3 BZ Equity","VALE5 BZ Equity",'VALE US Equity']} ,
+                                'LREN' :  { 'reportingccy': 'BRL','RelatedStockList': ['LREN3 BZ Equity','LRENY US Equity']},
+                                'NATURA' :  { 'reportingccy': 'BRL','RelatedStockList': ['NTCO3 BZ Equity', 'NTCO US Equity']},
+                                'ITAU':     { 'reportingccy': 'BRL','RelatedStockList': ['ITUB3 BZ Equity', 'ITUB4 BZ Equity','ITUB US Equity']}
 
-    print(LiquidationStock)
-    print(RelatedStockList,'remaining value of liquidation',reportingccy,round(LiquidationStock['position_'+reportingccy].sum()/1e9,1),'billion of holders selling at a liquidation pace of ',pace,'of their previous holdings')
+                    }
+
+    all_target_stocks,all_selling_stock = do_all_stocks(target_stock_dict, pace=-0.1)
+
+    recent_activity = recent_activity_calc(all_target_stocks, cutdate)
+
+    print(recent_activity)
+    print(all_selling_stock)
+    print(all_target_stocks)
+
+
+    #creating html for email
+    html_string1 = 'Most Recent Position Liquidation Reported for ' + str(list(target_stock_dict.keys())) +  recent_activity.to_html() + '<br>'+ '<br>'
+
+    html_string2 = 'Full Liquidation Volume for Holders who have been cutting at least ' + str(pace) + 'of position'   + all_selling_stock.to_html() + '<br>'+ '<br>'
+
+    html_string3 = 'Liquidation Volume per Holder who have been cutting at least '+ str(pace) + 'of position' + all_target_stocks.to_html() + '<br>'+ '<br>'
+
+    html_string = html_string1 + html_string2 + html_string3
+
+    subject = 'Large Equity Liquidations ' + str(cutdate)
+    # mail_to = 'mesa@vistacapital.com.br'
+    mail_to = 'abraga@vistacapital.com.br'
+
+    EmailSender.send_email_simple (mail_to=mail_to,subject=subject,bodymsg='hi',html_body=html_string,attachment=False)
+
+
+
+
+    # print(RelatedStockList,'remaining value of liquidation',reportingccy,LiquidationStock['position_'+reportingccy+'_mm'].sum(),'mm of holders selling at a liquidation pace of ',pace,'of their previous holdings')
 

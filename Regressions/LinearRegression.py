85cee922b9e436f2d5e5eba980de4a38d1f3f3db
ArthurBraga <abraga@vistacapital.com.br>
2020-12-02 00:36:32 -0500

    more stuff

--- Regressions/LinearRegression.py
+++ Regressions/LinearRegression.py
@@ -47,26 +47,28 @@ def rolling_regression(y, x, window=60):
             X_slice = X.values[i-window:i,:] # always index in np as opposed to pandas, much faster
             y_slice = y.values[i-window:i]
             coeff = np.dot(np.dot(np.linalg.inv(np.dot(X_slice.T, X_slice)), X_slice.T), y_slice)
-            est = coeff[0] * x.values[window-1] + coeff[1]
+            est = coeff[0] * X_slice[-1][0] + coeff[1]
             estimate_data.append(est)
             estimate_beta.append(coeff[0])
             estimate_alpha.append(coeff[1])
     # === Assemble ========================================================
         estimate = pd.Series(data=estimate_data, index=x.index[window-1:]).to_frame()
         estimate.columns = ['model']
-        estimate_beta = pd.Series(data=estimate_beta, index=x.index[window - 1:]).to_frame()
-        estimate_beta.columns = ['beta']
 
-        estimate_alpha = pd.Series(data=estimate_alpha, index=x.index[window - 1:]).to_frame()
-        estimate_alpha.columns = ['alpha']
-        estimate = pd.concat([estimate,estimate_beta,estimate_alpha],axis=1,sort=True)
+        estimate_beta_final = pd.Series(data=estimate_beta, index=x.index[window - 1:]).to_frame()
+        estimate_beta_final.columns = ['beta']
+
+        estimate_alpha_final = pd.Series(data=estimate_alpha, index=x.index[window - 1:]).to_frame()
+        estimate_alpha_final.columns = ['alpha']
+
+        estimate = pd.concat([estimate,estimate_beta_final,estimate_alpha_final],axis=1,sort=True)
 
     return estimate
 
 
-StartDate = 20200101
-EndDate = 20201123
-
+StartDate = 20190101
+EndDate = 20201201
+window = 66
 
 RegressionDict_bbg = {  'vix_spx' :
                                 {'y' : "VIX Index", 'x' : "SPX Index"},
@@ -97,52 +99,55 @@ AllPrices = EQ_FetchHistoricalPrices.pull_price_history(all_assets,StartDate=Sta
 AllPrices.columns = [element[0] for element in AllPrices.columns]
 
 
-regressions_dict = {}
-
+regressions_dict = dict()
 for item in RegressionDict_bbg.keys():
+    # item = 'vix_spx'
     print(item)
 
     xx = AllPrices[RegressionDict_bbg[item]['x']]
     x_name = RegressionDict_bbg[item]['x']
     yy = AllPrices[RegressionDict_bbg[item]['y']]
     y_name = RegressionDict_bbg[item]['y']
-
-    plt.scatter(xx,yy)
-
-    yy_est = rolling_regression(yy, xx, window=30)
+    # plt.scatter(xx,yy)
+    # z = np.polyfit(xx, yy, 1)
+    # p = np.poly1d(z)
+    # plt.plot(xx, p(xx), "r--")
+    plt.show()
+    yy_est = rolling_regression(y=yy, x=xx, window=window)
     yy_est.columns = [y_name +'-' +name for name in yy_est.columns]
-
+    df_model = pd.DataFrame()
     df_model = pd.concat([yy_est,yy],sort=True,axis=1)
-
-    df_model['residual'] = df_model[y_name].values - df_model[y_name+'-model']
-
+    df_model['residual'] = (df_model[y_name] - df_model[y_name+'-model'].values).to_frame()
     df_model[x_name] = xx
-
-    regressions_dict[item] = { 'x':x_name , 'y': y_name,'data': df_model}
+    regressions_dict[item] = dict({'x':x_name , 'y': y_name,'data': df_model})
 
 
 
 #prepping graph
-for item in regressions_dict.keys():
+for item in RegressionDict_bbg.keys():
 
     img_format = 'png'
-    residual = regressions_dict[item]['data']['residual'].to_frame()
+    residual = regressions_dict[item]['data']['residual'].to_frame().dropna()
+
     y_name = nickname_table[regressions_dict[item]['y']]
     x_name = nickname_table[regressions_dict[item]['x']]
     residual['zero'] = 0
     residual.plot()
+    plt.title(y_name + ' minus model vs. ' + x_name + ' - rolling '+str(window)+' biz days')
+    plt.xticks(rotation=45)
+
     f = io.BytesIO()
     filename = "C:\\Users\\ArthurBraga\\PycharmProjects\\Vista\\dump\\file_delete.png"
-    plt.savefig(filename)
+    plt.savefig(filename,bbox_inches='tight')
     f.seek(0)
     img_data = f.read()
     plt.close()
 
     #creating html for email
     html_string = residual.to_html()
-    subject = 'Regression & Residuals for ' + y_name + ' vs. '  + x_name+ +' - '+str(EndDate)
-    # mail_to = 'mesa@vistacapital.com.br'
+    subject = 'Regression & Residuals for ' + y_name + ' vs. '  + x_name+ ' - '+str(EndDate)
+    mail_to = 'mesa@vistacapital.com.br'
     mail_to = 'abraga@vistacapital.com.br'
-
-    EmailSender.send_email_embed_img(mail_to=mail_to, sender=mail_to, subject=subject, html_body='', image_path=filename)
+    mail_from = 'abraga@vistacapital.com.br'
+    EmailSender.send_email_embed_img(mail_to=mail_to, sender=mail_from, subject=subject, html_body='', image_path=filename)
     os.remove(filename)
